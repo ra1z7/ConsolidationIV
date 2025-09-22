c7b5bc4859cc15c83ed959561c6d1522aa996fff
ra1z <purnamaanrai7@gmail.com>
2025-09-22 17:37:13 +0545

    Make Simple Gender Selector

--- ConsolidationIV/ContentView.swift
+++ ConsolidationIV/ContentView.swift
@@ -19,6 +19,70 @@ struct ContentView: View {
     }
 }
 
+// Why @State works with structs
+struct Gender {
+    var gender: String
+    
+    init(gender: String = "Male") {
+        self.gender = gender
+    }
+}
+
+struct GenderSelectView: View {
+    @State private var gender = Gender()
+    
+    var genderIcon: String {
+        if gender.gender == "Male" {
+            return "figure.stand"
+        } else {
+            return "figure.stand.dress"
+        }
+    }
+    
+    var genderIconColor: Color {
+        if gender.gender == "Male" {
+            return .blue
+        } else {
+            return .pink
+        }
+    }
+
+    var body: some View {
+        VStack(spacing: 0) {
+            Image(systemName: genderIcon)
+                .font(.system(size: 100))
+                .foregroundStyle(genderIconColor)
+                .contentTransition(.symbolEffect(.replace))
+                .padding(.top)
+            
+            Rectangle()
+                .fill(.secondary)
+                .frame(width: 175, height: 0.5)
+
+            HStack {
+                makeButton(for: "Male", ofColor: .blue)
+                makeButton(for: "Female", ofColor: .pink)
+            }
+            .padding()
+        }
+        .overlay {
+            RoundedRectangle(cornerRadius: 30)
+                .stroke(.secondary, lineWidth: 0.5)
+        }
+    }
+    
+    func makeButton(for genderName: String, ofColor color: Color) -> some View {
+        Button {
+            gender.gender = genderName
+        } label: {
+            Text(genderName)
+                .frame(width: 60)
+        }
+        .buttonStyle(.bordered)
+        .tint(color)
+    }
+}
+
 #Preview {
-    ContentView()
+    GenderSelectView()
 }
