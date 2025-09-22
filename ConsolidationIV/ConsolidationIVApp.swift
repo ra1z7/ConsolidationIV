e689900b95d4866d9a95ab9e6aa477b513a4c172
ra1z <purnamaanrai7@gmail.com>
2025-09-22 16:30:20 +0545

    Initial Commit

---
+++ ConsolidationIV/ConsolidationIVApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConsolidationIVApp.swift
+//  ConsolidationIV
+//
+//  Created by Purnaman Rai (College) on 22/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct ConsolidationIVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
